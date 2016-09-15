c80ff73c0b8b88bc667c18eb802a60473fff567e
LearnInovationNature <1003156350@qq.com>
2016-09-16 07:47:36 +0800

    add notes to bubble-sort.py, and add line to READ.me to expain the bubble-sort.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # visual_algorithm_ds
 have fun with visually seeing the normal algorithms execute(ps: a better way to learn)
+1. bubble-sort: using bar plot to show how bubble sort is executed.
--- bubble-sort.py
+++ bubble-sort.py
@@ -12,11 +12,14 @@ x = np.linspace(0, 100, data_len)
 ch_input = ''
 x = np.arange(data_len)
 rect = None
-
+frame = 1
+fig = plt.figure()
+is_swaped = False
 for i in range(data_len):
     if ch_input == 'q':
         break;
     for j in range(i+1, data_len):
+        is_swaped = False
         bar_color = []
         for i1 in range(data_len):
             bar_color.append('g')
@@ -25,17 +28,20 @@ for i in range(data_len):
             data[j] = data[i]
             data[i] = swp
             bar_color[i] = bar_color[j] = 'r'
-        ch_input = raw_input('input your choice: q for quit, n for next step:')
-        ch_input = ch_input.strip()
-        if ch_input == 'n':
-            if rect:
-                rect.remove()
-            rect = plt.bar(x, data, 0.9, color = bar_color)
-            plt.axis([0, data_len + 2, 0, 1.2])
-            plt.show(block=False)
-        text.Annotation('hello', (3, 1.0))
-        if ch_input == 'q':
-            break;
+            is_swaped = True
+        if is_swaped:
+            ch_input = raw_input('input your choice: q for quit, n for next step:')
+            ch_input = ch_input.strip()
+            if ch_input == 'n': 
+                if rect:
+                    rect.remove()
+                rect = plt.bar(x, data, 0.9, color = bar_color)
+                plt.axis([0, data_len + 2, 0, 1.2])
+                plt.show(block=False)
+            #    fig.savefig(str(frame) + '.png')
+                frame += 1
+            if ch_input == 'q':
+                break;
 while True:
     ch_input = raw_input('algorithm is done, press q to quit:')
     if ch_input == 'q':
